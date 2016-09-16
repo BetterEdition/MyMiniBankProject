aa5963bcb16cd0240b5f2c1daf0f338fe439b010
Bent H. Pedersen <BHP@easv.dk>
2016-09-16 12:58:56 +0200

    Added BankAccountDBRepository (Not completed)

---
+++ Repositories/BankAccountDBRepository.cs
@@ -0,0 +1,43 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    class BankAccountDBRepository : IRepository<IBankAccount, int>
+    {
+        private List<IBankAccount> accounts = new List<IBankAccount>();
+
+        public int Count
+        {
+            get
+            {
+                return accounts.Count;
+            }
+        }
+
+        public void Add(IBankAccount e)
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public IList<IBankAccount> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IBankAccount GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(IBankAccount e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
